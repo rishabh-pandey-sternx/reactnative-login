396160f2526c314e505a41286cb16010ed5c930c
Rishabh <rishabh@geekyants.com>
2019-10-19 22:40:30 +0530

    changes

--- App.js
+++ App.js
@@ -1,19 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Platform, SafeAreaView } from 'react-native';
+import Auth0 from 'react-native-auth0';
+import * as AppAuth from 'expo-app-auth';
+import config from './config';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const auth0 = new Auth0({
+  domain: config.domain,
+  clientId: config.clientId
 });
+
+const bundleIdentifier = 'expo.app.auth.test';
+
+const auth0ClientId = config.clientId;
+const auth0Domain = config.domain;
+
+const config = {
+  issuer: `https://${auth0Domain}`,
+  scopes: ['openid profile'],
+  clientId: auth0ClientId,
+  redirectUrl: `${bundleIdentifier}://${auth0Domain}/${Platform.OS}/${bundleIdentifier}/callback`
+};
+
+export default class App extends React.Component {
+  login = () => {
+    console.log('config:', JSON.stringify(config, null, 2));
+
+    AppAuth.authAsync(config)
+      .then(response => console.log('resolved', response))
+      .catch(error => console.log('error', error));
+  };
+  onLogin = () => {
+    auth0.webAuth
+      .authorize({
+        scope: 'openid profile email',
+        audience: 'https://' + auth0Domain + '/userinfo'
+      })
+      .then(credentials => {
+        console.log(credentials, 'credentials');
+      })
+      .catch(error => console.log(error));
+  };
+
+  render() {
+    return (
+      <SafeAreaView>
+        <Button title="Login" onPress={this.onLogin} />
+      </SafeAreaView>
+    );
+  }
+}
---
+++ App2.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { AuthSession } from 'expo';
+import * as Auth0 from 'react-native-auth0';
+import jwtDecode from 'jwt-decode';
+import config from './config';
+
+/*
+  You need to swap out the Auth0 client id and domain with
+  the one from your Auth0 client.
+  In your Auth0 client, you need to also add a url to your authorized redirect urls.
+  For this application, I added https://auth.expo.io/@arielweinberger/auth0-example because I am
+  signed in as the "community" account on Expo and the slug for this app is "auth0-example" (check out app.json).
+  You can open this app in the Expo client and check your logs to find out your redirect URL.
+*/
+const auth0ClientId = config.clientId;
+const auth0Domain = config.domain;
+
+/**
+ * Converts an object to a query string.
+ */
+function toQueryString(params) {
+  return (
+    '?' +
+    Object.entries(params)
+      .map(
+        ([key, value]) =>
+          `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+      )
+      .join('&')
+  );
+}
+
+export default class App extends React.Component {
+  state = {
+    name: null
+  };
+
+  login = async () => {
+    // Retrieve the redirect URL, add this to the callback URL list
+    // of your Auth0 application.
+    const redirectUrl = AuthSession.getRedirectUrl();
+    console.log(`Redirect URL: ${redirectUrl}`);
+
+    // Structure the auth parameters and URL
+    const queryParams = toQueryString({
+      client_id: auth0ClientId,
+      redirect_uri: redirectUrl,
+      response_type: 'id_token', // id_token will return a JWT token
+      scope: 'openid profile', // retrieve the user's profile
+      nonce: 'nonce' // ideally, this will be a random value
+    });
+    const authUrl = `${auth0Domain}/authorize` + queryParams;
+
+    // Perform the authentication
+    const response = await AuthSession.startAsync({ authUrl });
+    console.log('Authentication response', response);
+
+    if (response.type === 'success') {
+      this.handleResponse(response.params);
+    }
+  };
+
+  handleResponse = response => {
+    if (response.error) {
+      Alert(
+        'Authentication error',
+        response.error_description || 'something went wrong'
+      );
+      return;
+    }
+
+    // Retrieve the JWT token and decode it
+    const jwtToken = response.id_token;
+    const decoded = jwtDecode(jwtToken);
+
+    const { name } = decoded;
+    this.setState({ name });
+  };
+
+  render() {
+    const { name } = this.state;
+
+    return (
+      <View style={styles.container}>
+        {name ? (
+          <Text style={styles.title}>You are logged in, {name}!</Text>
+        ) : (
+          <Button title="Log in with Auth0" onPress={this.login} />
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 40
+  }
+});
